11d7bd1e5c0e103caa7e7177120cd9d3b64bc670
Max Goodman <max.goodman@flatironschool.com>
2018-06-22 08:27:15 -0400

    editing max path

---
+++ app/controllers/max_controller.rb
@@ -0,0 +1,6 @@
+class MaxController < ApplicationController
+	def index
+	end
+	def show
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :charges
   root 'charges#index'
-  get '/max' => 'charges#max'
+  get "max" => "max#index"
+  resources :max
 end
